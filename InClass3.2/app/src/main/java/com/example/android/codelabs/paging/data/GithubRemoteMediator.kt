9d908746e7bd9cadcf3cf40631ed65c5e11a6cf6
Hippopen <baoxuannguyen2005@gmail.com>
2026-04-20 23:00:59 +0700

    Complete InClass Exercises

---
+++ InClass3.2/app/src/main/java/com/example/android/codelabs/paging/data/GithubRemoteMediator.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.codelabs.paging.data
+
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.LoadType
+import androidx.paging.PagingState
+import androidx.paging.RemoteMediator
+import com.example.android.codelabs.paging.api.GithubService
+import com.example.android.codelabs.paging.db.RepoDatabase
+import com.example.android.codelabs.paging.model.Repo
+
+@OptIn(ExperimentalPagingApi::class)
+class GithubRemoteMediator(
+    private val query: String,
+    private val service: GithubService,
+    private val repoDatabase: RepoDatabase
+) : RemoteMediator<Int, Repo>() {
+
+    override suspend fun load(loadType: LoadType, state: PagingState<Int, Repo>): MediatorResult {
+        TODO("Not yet implemented")
+    }
+}
